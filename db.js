a077cd1c52054f6beff42aa6789e715d71b46e54
Tambe285 <78977120+Tambe285@users.noreply.github.com>
2026-06-06 04:49:31 +0530

    Add checkServerIdentity to SSL configuration

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  ssl: { rejectUnauthorized: false, checkServerIdentity: () => undefined },
   max: 5,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 10000,
