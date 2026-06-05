baa2ebe3a9e0743f35c1519dddbed5e95e875ba8
Tambe285 <78977120+Tambe285@users.noreply.github.com>
2026-06-05 05:46:46 +0530

    Fix server.js to serve index.html

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 require('dotenv').config();
 
 const app = express();
