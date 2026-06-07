ca121c9c42240f7cfb5fdc80ccc12da5f1cdfba7
Tambe285 <78977120+Tambe285@users.noreply.github.com>
2026-06-07 07:27:44 +0530

    Fix formatting in emailService.js

--- src/utils/emailService.js
+++ src/utils/emailService.js
@@ -4,8 +4,8 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: process.env.EMAIL_USER, // Your Gmail address
-    pass: process.env.EMAIL_PASS  // Your Gmail App Password
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
   }
 });
 
