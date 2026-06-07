7dc59754bdc2babd863165864b16c7aeb42cf9d9
Tambe285 <78977120+Tambe285@users.noreply.github.com>
2026-06-07 07:36:18 +0530

    Comment out email service import
    Comment out the import of sendBusinessApprovalEmail.

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { query } = require('../db');
-const { sendBusinessApprovalEmail } = require('../utils/emailService');
+// const { sendBusinessApprovalEmail } = require('../utils/emailService');
 
 // ==========================================
 // 1. ADMIN LOGIN ROUTE
